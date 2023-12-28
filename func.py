12bd5a84c81188b2e21eefeee5557c75b91b3941
Muhammad Sufyan <shakirali9749@gmail>
2023-12-28 05:16:07 -0800

    add the dacorator

---
+++ func.py
@@ -0,0 +1,56 @@
+# def shout():
+# 	return "source code".upper()
+# def whisper():
+# 	return "source code".lower()
+# def display(fun):
+# 	print(fun())
+# print(display(shout))
+# print(display(whisper))
+
+# def get_age_in_month(age):
+# 	def show():
+# 		print('i am an inner fun')
+# 	return show 
+# get_age_in_month(25)()
+
+
+# def code():
+# 	return 'ali is my best frind'.upper()
+# def source():
+# 	return 'Ahmad is my best frind'.lower()
+# def junu(spna):
+# 	print(spna())
+# print(code())
+# print(source())
+# print(junu(code))
+
+def shout(text):
+    return text.upper()
+ 
+print(shout('Hello'))
+ 
+yell = shout
+ 
+print(yell('broker'))
+
+# def door(learn):
+# 	return learn.upper()
+# 	return learn.lower
+# print(door("add the new function"))
+# yell=door
+# print(yell("door the new learn"))
+
+
+def close(test):
+	return test.upper()
+print(close("hello fatma"))
+
+
+
+
+
+
+
+
+
+
