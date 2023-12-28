bef07014fd6786f0e729cadce8f1edf63b56d480
Muhammad Sufyan <shakirali9749@gmail>
2023-12-28 05:25:29 -0800

    add the decotor in this file

--- func.py
+++ func.py
@@ -1,28 +1,28 @@
-# def shout():
-# 	return "source code".upper()
-# def whisper():
-# 	return "source code".lower()
-# def display(fun):
-# 	print(fun())
-# print(display(shout))
-# print(display(whisper))
+def shout():
+	return "source code".upper()
+def whisper():
+	return "source code".lower()
+def display(fun):
+	print(fun())
+print(display(shout))
+print(display(whisper))
 
-# def get_age_in_month(age):
-# 	def show():
-# 		print('i am an inner fun')
-# 	return show 
-# get_age_in_month(25)()
+def get_age_in_month(age):
+	def show():
+		print('i am an inner fun')
+	return show 
+get_age_in_month(25)()
 
 
-# def code():
-# 	return 'ali is my best frind'.upper()
-# def source():
-# 	return 'Ahmad is my best frind'.lower()
-# def junu(spna):
-# 	print(spna())
-# print(code())
-# print(source())
-# print(junu(code))
+def code():
+	return 'ali is my best frind'.upper()
+def source():
+	return 'Ahmad is my best frind'.lower()
+def junu(spna):
+	print(spna())
+print(code())
+print(source())
+print(junu(code))
 
 def shout(text):
     return text.upper()
@@ -33,17 +33,37 @@ yell = shout
  
 print(yell('broker'))
 
-# def door(learn):
-# 	return learn.upper()
-# 	return learn.lower
-# print(door("add the new function"))
-# yell=door
-# print(yell("door the new learn"))
+def door(learn):
+	return learn.upper()
+	return learn.lower
+print(door("add the new function"))
+yell=door
+print(yell("door the new learn"))
 
 
 def close(test):
 	return test.upper()
 print(close("hello fatma"))
+
+def outer(f):
+	def inner():
+		print('before func call')
+
+		f()
+
+		print('after func call')
+
+	return inner
+
+@outer
+def passed_func():
+	print('i am passed into a func')
+
+outer(passed_func)()
+
+passed_func = outer(passed_func)
+
+passed_func()
 
 
 
