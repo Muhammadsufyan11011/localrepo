8a0cd1e6e497af164d2c9ac8f0e33c0d624aa4cd
Muhammad Sufyan <shakirali9749@gmail>
2024-01-01 22:24:58 -0800

    add the while loop in this file

--- func.py
+++ func.py
@@ -1,69 +1,101 @@
-def shout():
-	return "source code".upper()
-def whisper():
-	return "source code".lower()
-def display(fun):
-	print(fun())
-print(display(shout))
-print(display(whisper))
+# def shout():
+# 	return "source code".upper()
+# def whisper():
+# 	return "source code".lower()
+# def display(fun):
+# 	print(fun())
+# print(display(shout))
+# print(display(whisper))
 
-def get_age_in_month(age):
-	def show():
-		print('i am an inner fun')
-	return show 
-get_age_in_month(25)()
+# def get_age_in_month(age):
+# 	def show():
+# 		print('i am an inner fun')
+# 	return show 
+# get_age_in_month(25)()
 
 
-def code():
-	return 'ali is my best frind'.upper()
-def source():
-	return 'Ahmad is my best frind'.lower()
-def junu(spna):
-	print(spna())
-print(code())
-print(source())
-print(junu(code))
+# def code():
+# 	return 'ali is my best frind'.upper()
+# def source():
+# 	return 'Ahmad is my best frind'.lower()
+# def junu(spna):
+# 	print(spna())
+# print(code())
+# print(source())
+# print(junu(code))
 
-def shout(text):
-    return text.upper()
+# def shout(text):
+#     return text.upper()
  
-print(shout('Hello'))
+# print(shout('Hello'))
  
-yell = shout
+# yell = shout
  
-print(yell('broker'))
+# print(yell('broker'))
 
-def door(learn):
-	return learn.upper()
-	return learn.lower
-print(door("add the new function"))
-yell=door
-print(yell("door the new learn"))
+# def door(learn):
+# 	return learn.upper()
+# 	return learn.lower
+# print(door("add the new function"))
+# yell=door
+# print(yell("door the new learn"))
 
 
-def close(test):
-	return test.upper()
-print(close("hello fatma"))
+# def close(test):
+# 	return test.upper()
+# print(close("hello fatma"))
 
-def outer(f):
-	def inner():
-		print('before func call')
 
-		f()
 
-		print('after func call')
 
-	return inner
 
-@outer
-def passed_func():
-	print('i am passed into a func')
 
-outer(passed_func)()
 
-passed_func = outer(passed_func)
+# def outer(f):
+# 	def inner():
+# 		print('before func call')
 
-passed_func()
+# 		f()
+
+# 		print('after func call')
+
+# 	return inner
+
+# @outer
+# def passed_func():
+# 	print('i am passed into a func')
+
+# outer(passed_func)()
+
+# passed_func = outer(passed_func)
+
+# passed_func()
+
+
+
+
+# def outer(code):
+# 	def inner():
+# 		print('i can before the call')
+# 		code()	
+# 		print('i can after the file')
+# 	return inner
+# def lunch():
+# 	print('i am first the breakfast')
+# lunch()
+
+
+
+def food(roti):
+	def slan():
+		print('mix the slan and roti')
+		roti()
+		
+
+
+
+
+
 
 
 
---
+++ test.py
@@ -0,0 +1,126 @@
+name=22
+if name=='khan':
+	print('logging to this page')
+else:
+	print('this page is not logging')
+print('this page is loggging')
+name='ali'
+if name=='ali':
+	print('this name is ali')
+else:
+	print('this name is not ali')
+print('this code will be not executed')
+
+if name=='harry':
+	print('hello harry')
+if password=='code':
+	print('accesed code with harry')
+else:
+	print('wrong password')
+name = 'sharda'
+if name== 'sharda':
+	print('this is the nmae is printed')
+else:
+	print('this is not use name')
+
+name ='khan'
+if name=='jan':
+	print('this is the name code')
+else age<5:
+	print('this code is not excuted')
+
+spam=0
+if spam<5:
+	print('this code will be executed')
+	spam=spam+1
+print(spam)
+
+spam=0
+while spam<5:
+	print('hello my name is sufyab')
+	spam=spam+1
+
+i=1
+while i<=10:
+	print(i,'my name is ali')
+	i=i+1
+print(i)
+
+i=10
+while i>=1:
+	print(i,'hello my name is sufyab')
+	i=i-1
+print(i)
+
+
+name=''
+while name != 'your name':
+	print('please type your name: ')
+	name=input()
+print('thank your ')
+
+name=''
+while name!='you name':
+	print('what is your name')
+	name=input()
+print('thank you')
+while True:
+	print('please type your name')
+	name=input()
+	if name=='your name':
+		break
+print('thank you')
+
+while True:
+	print('what is your name:')
+	name =input()
+	if name=='sufyan':
+		break
+print('hello my name is sufyan')
+
+number=0
+for a in range(10):
+	number=a+1
+	if number==5:
+		break
+	print(number)
+print('out of the code')
+
+number=0
+for code in range(30):
+	number=code+1
+	if number==7:
+		continue
+	print(number)
+print('out of there code will be execute')
+number=0
+for i in range(10):
+	number=i+1
+	if number==8:
+		continue
+	print(number)
+print('out of the code')
+number=0
+for i in range(44):
+	number=i+1
+	if number==8:
+		continue
+	print(number)
+print('this code will execute')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
