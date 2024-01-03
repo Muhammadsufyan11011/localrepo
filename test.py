b1a63c414398094d6d4de591158b04dacc95cb5c
Muhammad Sufyan <shakirali9749@gmail>
2024-01-03 09:45:30 -0800

    add the while loop and for loop in this code

--- test.py
+++ test.py
@@ -107,9 +107,153 @@ for i in range(44):
 		continue
 	print(number)
 print('this code will execute')
+while True:
+	print('who are you?')
+	name=input()
+	if name!='joe':
+		continue
+	print('hello,joe.what is password?(it is a fish.)')
+	password=input()
+	if password=='swordfish':
+		break
+print('Accesss granted.')
+while True:
+	print('what is your name:')
+	name=input()
+	if name!='sufyan':
+		continue
+	print('hello frind this is my first code')
+	password=input()
+	if password=='111':
+		break
+print('access the all code')
+while True:
+	print('who are you:')
+
+	name=input()
+
+	if name=='khan':
+		continue
+	print('this code will be explanne')
+	password=input()
+	if password=='ali':
+		break
+print('access the all code')
+
+ 
+
+print("hello world")
+name=input('what is your name:')
+print(f'hello,{name}!nice to meet you')
+
+name =input('what is your name:')
+print(f"my name is {name}")
+
+
+name=input('what is your name?')
+if name=='Alice'or name=='Bob':
+	print(f"Hello my name is {name}! you Good luck")
+else:
+	print('sorry yor are not code alice and bob')
+
+n=int(input("Enter a number :"))
+sum_of_number=sum(range(1,n+1))
+print(f'the sum of number form 1 to {n}is :{sum_of_number}')
+
+n=int(input("Enter a number (n):"))
+sum_of_multipul=sum(x for x in range(1,n+1) if x%3==0 or x%5==0)
+print(f'the sum of multiple of three or five form 1  to {n} is:{sum_of_multipul}')
+
+while True:
+
+	print('Who are you?')
+
+	name = input()
+ 	if name != 'Joe':
+		continue
+	print('Hello, Joe. What is the password? (It is a fish.)')
+	password = input()
+	if password == 'swordfish':
+		break
+ print('Access granted.') 
+while True:
+	print('who are you:')
+	name=input()
+	if name !='ali':
+		continue
+	print('this code will execute ')
+	password=input()
+	if password=='jeo':
+		break
+	print('where this source code')
+print('this code will end')
+count=0
+while (count<3):
+	count=count+1
+	print(count,'hello world')
+while True:
+	try:
+		age=int(input('Enter your age:'))
+		break
+	except ValueError:
+		print('Invalid input,please enter a valid input')
+if age < 18:
+	print('yor are a minor')
+else:
+	print('you are adault')
+while True:
+	try:
+		age =int(input('Enter your agge:'))
+		break
+	except ValueError:
+		print('the input is invalid')
+if age <18:
+	print('this is my age is valuid')
+else:
+	print('this age is not valuid')
+
+i= 0
+a='greekforgreek'
+while i < len(a):
+	if a[i] == 'e' or a[i] == 's':
+		i +=1
+		continue
+	print('Current Letter:',a[i])
+	i+=1
+
+i = 0
+a = 'geeksforgeeks'
+  
+while i < len(a): 
+    if a[i] == 'e' or a[i] == 's': 
+        i += 1
+        continue
+          
+    print('Current Letter :', a[i]) 
+    i += 1
 
 
 
+print('My name is')
+for i in range(5):
+ print('Jimmy Five Times (' + str(i) + ')')
+for n in range(10):
+	print(n)
+for n in range(1,6,3):
+	print(n)
+for a in range(1,11):
+	print('2*',a,'=',2*a)
+for i in range(5):
+	print('sufyan('+str(i) +')')
+	print('sufyan is my best frind(' +str(i) +')')
+total=0
+for num in range(10):
+	total=total+num
+	print(total)
+for b in range(1,11):
+	print('3*',b,'=',3*b)
+for a in range(10,-4,-1):
+	print(a)
 
 
 
